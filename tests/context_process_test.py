306c1a40ada94fa53f11b8fe7de29566c4802cc4
komaluntwal <untwalkomal13@gmail.com>
2022-03-07 23:53:03 -0500

    added 4 pages

--- tests/context_process_test.py
+++ tests/context_process_test.py
@@ -1,30 +1,22 @@
 import datetime
 from os import getenv
 
-def test_context_variables_environment(client):
-    """This test checks if the environment is printed"""
+
+def test_footer_text(client):
+    """This test checks the footer text in home page"""
     response = client.get("/")
-    env = getenv('FLASK_ENV', None)
-    test_string = f"Environment: {env}"
+    test_string = "My Tech Manual"
     content = bytes(test_string, 'utf-8')
     assert response.status_code == 200
     assert content in response.data
 
-def test_context_variables_year(client):
-    """This tests checks if the copyright and current year are printed"""
+
+def test_footer_text_year(client):
+    """This tests checks if current year is printed in footer text"""
     response = client.get("/")
     current_date_time = datetime.datetime.now()
     date = current_date_time.date()
     year = date.strftime("%Y")
-    test_string = f"Copyright: {year}"
-    content = bytes(test_string, 'utf-8')
-    assert response.status_code == 200
-    assert content in response.data
-
-def test_context_currency_format(client):
-    """This tests checks if the copyright and current year are printed"""
-    response = client.get("/")
-    test_string = f"$100"
-    content = bytes(test_string, 'utf-8')
+    content = bytes(year, 'utf-8')
     assert response.status_code == 200
     assert content in response.data
--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,50 +1,50 @@
 """This test the homepage"""
 
+
 def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/about">About</a>' in response.data
-    assert b'<a class="nav-link" href="/page1">Page 1</a>' in response.data
-    assert b'<a class="nav-link" href="/page2">Page 2</a>' in response.data
-    assert b'<a class="nav-link" href="/page3">Page 3</a>' in response.data
-    assert b'<a class="nav-link" href="/page4">Page 4</a>' in response.data
+    assert b'<a class="nav-link" href="/git">Git</a>' in response.data
+    assert b'<a class="nav-link" href="/docker">Docker</a>' in response.data
+    assert b'<a class="nav-link" href="/flask">Flask</a>' in response.data
+    assert b'<a class="nav-link" href="/cicd">CI/CD</a>' in response.data
+
 
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Index Page" in response.data
+    assert b"" in response.data
 
-def test_request_about(client):
-    """This makes the index page"""
-    response = client.get("/about")
-    assert response.status_code == 200
-    assert b"About Page" in response.data
 
 def test_request_page1(client):
     """This makes the index page"""
-    response = client.get("/page1")
+    response = client.get("/git")
     assert response.status_code == 200
-    assert b"Page 1" in response.data
+    assert b"Git : Basic Manual" in response.data
+
 
 def test_request_page2(client):
     """This makes the index page"""
-    response = client.get("/page2")
+    response = client.get("/docker")
     assert response.status_code == 200
-    assert b"Page 2" in response.data
+    assert b"Docker" in response.data
+
 
 def test_request_page3(client):
     """This makes the index page"""
-    response = client.get("/page3")
+    response = client.get("/flask")
     assert response.status_code == 200
-    assert b"Page 3" in response.data
+    assert b"Flask : Web Framework" in response.data
+
 
 def test_request_page4(client):
     """This makes the index page"""
-    response = client.get("/page4")
+    response = client.get("/cicd")
     assert response.status_code == 200
-    assert b"Page 4" in response.data
+    assert b"CI/CD" in response.data
+
 
 def test_request_page_not_found(client):
     """This makes the index page"""
